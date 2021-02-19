9f7bba561f12ca8f6d5b91241145558213af94ca
karmavir joshi <karmavirj@live.com>
2021-02-19 23:05:08 +0530

    added few pages content samples in aabout us,  process and homepage.
    Also added pintrest to contact us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import {ProductCatComponent} from './site/product-cat/product-cat.component';
+import { ProductCatComponent } from './site/product-cat/product-cat.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './site/nav/nav.component';
