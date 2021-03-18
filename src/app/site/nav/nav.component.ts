a04a1cf005139e9bcf1d309c91f0c89c238fb804
Karmavir Joshi <karmavirj@live.com>
2021-03-18 13:53:48 +0530

    yark

--- src/app/site/nav/nav.component.ts
+++ src/app/site/nav/nav.component.ts
@@ -5,11 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("nav init");
   }
 
 }
--- src/app/site/site.routing.component.ts
+++ src/app/site/site.routing.component.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {SiteComponent} from './site.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {AboutUsComponent} from './about-us/about-us.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {ProcessComponent} from './process/process.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
+import {ProductCatComponent} from './product-cat/product-cat.component';
+import {HistoryComponent} from './history/history.component';
 
 const routes: Routes = [
   {
@@ -10,7 +15,13 @@ const routes: Routes = [
     component: SiteComponent,
     children: [
       { path: '', component: HomePageComponent },
-      { path: 'about-us', component: AboutUsComponent }
+      { path: 'about-us', component: AboutUsComponent },
+      { path: 'contact-us', component: ContactUsComponent },
+      { path: 'process', component: ProcessComponent },
+      { path: 'category', component: ProductCatComponent },
+      { path: 'product', component: ProductDetailsComponent },
+      { path: 'history', component: HistoryComponent }
+
     ]
 
   }
